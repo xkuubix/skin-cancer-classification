9b0695d903b5a0f4c84c4bfd67f190ef3b7fae57
Jakub Buler <buler.jakub@gmail.com>
2024-03-13 17:15:52 +0100

    added time logging and time convert function

--- RadiomicsExtractor.py
+++ RadiomicsExtractor.py
@@ -24,23 +24,34 @@ class RadiomicsExtractor():
         return self.extractor.execute(im, seg_path, label=label)
     
     def parallell_extraction(self, list_of_dicts: list, n_processes = None):
+        logger.info(f"Extracting radiomics features: mode-parallel")
         if n_processes is None:
             n_processes = multiprocessing.cpu_count() - 1
         start_time = time.time()
         with Pool(n_processes) as pool:
-            # results = pool.map(self.extract_radiomics, list_of_dicts, chunksize=None)
-            results = list(tqdm(pool.imap(self.extract_radiomics, list_of_dicts,
-                                          chunksize=n_processes),
+            results = list(tqdm(pool.imap(self.extract_radiomics, list_of_dicts),
                                  total=len(list_of_dicts)))
-
-
         end_time = time.time()
-        logger.info(f" {(end_time - start_time) / len(list_of_dicts):.3f} s/img")
-        logger.info(f" {end_time - start_time:.3f} s\n")
+
+        h, m, s = self._convert_time(start_time, end_time)
+        logger.info(f" Time taken: {h}h:{m}m:{s}s")
+
         return results
     
     def serial_extraction(self, list_of_dicts: list):
+        logger.info(f"Extracting radiomics features: mode-serial")
         all_results = []
             # for item in trange(len(train_df)):
+        start_time = time.time()
         for item in trange(len(list_of_dicts)):
             all_results.append(self.extract_radiomics(list_of_dicts[item]))
+        end_time = time.time()
+
+        h, m, s = self._convert_time(start_time, end_time)
+        logger.info(f" Time taken: {h}h:{m}m:{s}s")
+        return all_results
+
+    def _convert_time(self, start_time, end_time):
+        dt = end_time - start_time
+        h, m, s = int(dt // 3600), int((dt % 3600 ) // 60), int(dt % 60)
+        return h, m, s
