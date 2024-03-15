3697ecafc82e709b1307f45c875ff90d5f1cfdad
Jakub Buler <buler.jakub@gmail.com>
2024-03-15 13:28:10 +0100

    added operating mode, updated doc

--- Dataset.py
+++ Dataset.py
@@ -4,6 +4,7 @@ import logging
 
 import torch
 import pandas as pd
+import numpy as np
 from PIL import Image
 from collections import Counter
 from torch.utils.data import Dataset
@@ -15,42 +16,70 @@ logger = logging.getLogger(__name__)
 class HAM10000(Dataset):
     def __init__(self,
                  df: pd.DataFrame,
+                 mode: str,
                  transform=None):
+        """
+        Initializes the Dataset object.
+
+        Args:
+            df (pd.DataFrame): The input DataFrame containing the dataset.
+            mode (str): The mode of the dataset. Can be either 'images' or 'radiomics'.
+            transform (optional): The transformation to be applied to the dataset in 'images' mode.
+
+        Raises:
+            ValueError: If an invalid mode is provided.
+
+        """
         logger.info('Initialising ...')
-        
         self._df = df
         self._transform = transform
         self.mapping_handler = MappingHandler()
         msg = 'Finished initialising - class distribution:'
         msg += pretty_dict_str(Counter(self._df['dx']))
-
         logger.info(msg)
+        self.mode = mode
+        if self.mode not in ['images', 'radiomics']:
+            raise ValueError("Invalid mode")
+        logger.info(f'Dataset Mode: {self.mode}')
+
 
     def __getitem__(self, index):
         
-        # Load images from paths, map label
         image_path = self._df.iloc[index]['img_path']
         segmentation_path = self._df.iloc[index]['seg_path']
-
-        image = Image.open(image_path)
-        mask = Image.open(segmentation_path)
-
         label_str = self._df.iloc[index]['dx']
         label = torch.tensor(self.mapping_handler.convert(label_str))
+        if self.mode == 'images':
+            # Load images from paths, map label
 
-        if self._transform:
-            image = self._transform(image)
-            mask = self._transform(mask)
+            image = Image.open(image_path)
+            mask = Image.open(segmentation_path)
 
-        data_dict = {
-            'image': image,
-            'mask': mask,
-            'label': label,
-            'img_path': image_path,
-            'seg_path': segmentation_path,
-            'label_str': label_str
-            }
+            if self._transform:
+                image = self._transform(image)
+                mask = self._transform(mask)
 
+            data_dict = {
+                'image': image,
+                'mask': mask,
+                'label': label,
+                'img_path': image_path,
+                'seg_path': segmentation_path,
+                'label_str': label_str
+                }
+        elif self.mode == 'radiomics':
+
+            features = self._df.iloc[index].drop(self._df.columns[0:10])
+            features_names = self._df.columns[10:]
+            features = torch.tensor(np.array(features, dtype=np.float32), dtype=torch.float32)
+            data_dict = {
+                'label': label,
+                'img_path': image_path,
+                'seg_path': segmentation_path,
+                'label_str': label_str,
+                'features': features,
+                'features_names': features_names
+                }
         return data_dict
     
     def __len__(self):
