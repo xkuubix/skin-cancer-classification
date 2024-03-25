5e1d25e01fa8a2ef675b7ae9b370ed127a83b3dc
Jakub Buler <buler.jakub@gmail.com>
2024-03-25 14:19:55 +0100

    init (code from main)

---
+++ extract_radiomics.py
@@ -0,0 +1,126 @@
+# %% IMPORTS AND SETTINGS
+import yaml
+import pickle
+import os
+import torch
+import numpy as np
+import pandas as pd
+import utils
+import logging
+from RadiomicsExtractor import RadiomicsExtractor
+from Dataset import HAM10000
+import datetime
+import albumentations as A
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+logger_radiomics = logging.getLogger("radiomics")
+logger_radiomics.setLevel(logging.ERROR)
+
+# MAKE PARSER AND LOAD PARAMS FROM CONFIG FILE--------------------------------
+parser = utils.get_args_parser('config.yml')
+args, unknown = parser.parse_known_args()
+with open(args.config_path) as file:
+    config = yaml.load(file, Loader=yaml.FullLoader)
+
+# SET FIXED SEED FOR REPRODUCIBILITY --------------------------------
+seed = config['seed']
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+# %% DATA LOADING - 3-SET-SPLIT
+df = pd.read_csv(config['dir']['csv'])
+df = utils.insert_paths_df(df, config['dir']['img'], config['dir']['seg'])
+df = utils.group_df(df)
+
+train_df, val_df, test_df = utils.random_split_df(df,
+                                                  config['dataset']['split_fraction_train_rest'],
+                                                  config['dataset']['split_fraction_val_test'],
+                                                  seed=seed)
+# %%
+train_df = utils.ungroup_df(train_df)
+val_df = utils.ungroup_df(val_df)
+test_df = utils.ungroup_df(test_df)
+# %% RADIOMICS FEATURES EXTRACTION
+train_d = train_df.to_dict(orient='records')
+test_d = test_df.to_dict(orient='records')
+val_d = val_df.to_dict(orient='records')
+# %%
+
+if config['dataset']['train_sampling']['method'] == 'oversample':
+    train_d = utils.oversample_data(train_d)
+elif config['dataset']['train_sampling']['method'] == 'undersample':
+    train_d = utils.undersample_data(train_d, seed=seed, multiplier=config['dataset']['train_sampling']['multiplier'])
+elif config['dataset']['train_sampling']['method'] == 'none':
+    pass
+# %%
+if config['radiomics']['extract']:
+
+    transforms_train = A.Compose([
+        A.ToGray(p=1),
+        # A.RandomCrop(width=256, height=256),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.Rotate(limit=90, p=0.5),
+        A.RandomBrightnessContrast(p=0.5),
+
+        A.OneOf([
+            A.MedianBlur(blur_limit=3, p=0.5),
+            A.Blur(blur_limit=3, p=0.5),
+        ], p=0.25),
+        A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=45, p=0.5),
+        A.OneOf([
+            A.OpticalDistortion(p=0.5),
+            A.GridDistortion(p=0.5),
+        ], p=0.25),
+        A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=0.1, val_shift_limit=0.1, p=0.5),
+    ])
+    transforms_val_test = A.Compose([A.ToGray(p=1),])
+    transforms_train = A.Compose([A.ToGray(p=1, always_apply=True),])
+
+    extractor_train = RadiomicsExtractor(param_file='params.yml', transforms=transforms_train)
+    extractor_val_test = RadiomicsExtractor(param_file='params.yml', transforms=transforms_val_test)
+    if config['radiomics']['mode'] == 'parallel':
+        results_train = extractor_train.parallell_extraction(train_d)
+        results_val = extractor_val_test.parallell_extraction(val_d)
+        results_test = extractor_val_test.parallell_extraction(test_d)
+    elif config['radiomics']['mode'] == 'serial':
+        results_train = extractor_train.serial_extraction(train_d)
+        results_val = extractor_val_test.serial_extraction(val_d)
+        results_test = extractor_val_test.serial_extraction(test_d)
+    
+    train_df = pd.DataFrame(train_d)
+    train_df = pd.concat([train_df, pd.DataFrame(results_train)], axis=1)
+    val_df = pd.DataFrame(val_d)
+    val_df = pd.concat([val_df, pd.DataFrame(results_val)], axis=1)
+    test_df = pd.DataFrame(test_d)
+    test_df = pd.concat([test_df, pd.DataFrame(results_test)], axis=1)
+    
+    if config['radiomics']['save']:
+        with open(config['dir']['pkl_train'], 'wb') as handle:
+            pickle.dump(train_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(config['dir']['pkl_val'], 'wb') as handle:
+            pickle.dump(val_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(config['dir']['pkl_test'], 'wb') as handle:
+            pickle.dump(test_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        logger.info(f"Saved radiomics features in {config['dir']['pkl_train']}")
+        logger.info(f"Saved radiomics features in {config['dir']['pkl_val']}")
+        logger.info(f"Saved radiomics features in {config['dir']['pkl_test']}")
+        image_types = extractor_train.get_enabled_image_types()
+        feature_types = extractor_train.get_enabled_features()
+        with open(config['dir']['inf'], 'w') as file:
+            current_datetime = datetime.datetime.now()
+            file.write(f"Modified: {current_datetime}\n")
+            file.write(yaml.dump(config))
+            file.write('\n\nEnabled Image Types:\n')
+            file.write('\n'.join(image_types))
+            file.write('\n\nEnabled Features:\n')
+            file.write('\n'.join(feature_types))
+            file.write('\n\nTransforms:\n' + str(transforms_train))
+        logger.info(f"Saved extraction details in {config['dir']['inf']}")
+# %%
+train_ds = HAM10000(df=train_df, mode='radiomics')
+val_ds = HAM10000(df=val_df, mode='radiomics')
+test_ds = HAM10000(df=test_df, mode='radiomics')
+# %%
