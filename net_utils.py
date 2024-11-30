5630d5b5f506cc67b8e2ceb3e2683c99116f6e85
Jakub Buler <buler.jakub@gmail.com>
2024-11-30 18:47:41 +0100

    added handling dataset modes

--- net_utils.py
+++ net_utils.py
@@ -1,7 +1,6 @@
 import time
 import torch
-from sklearn.metrics import classification_report
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, recall_score, balanced_accuracy_score
 
 
 def one_hot(x, length):
@@ -14,6 +13,7 @@ def one_hot(x, length):
 def train_net(net, train_dl, val_dl, criterion, optimizer, n_classes, config, device):
     net.to(device)
     best_val_loss = float('inf')
+    best_val_metric = 0.
     best_model = None
     patience = config['net_train']['patience']
     early_stop_counter = 0
@@ -25,15 +25,22 @@ def train_net(net, train_dl, val_dl, criterion, optimizer, n_classes, config, de
         correct = 0
         total = 0
         for i, data in enumerate(train_dl):
-            image = data['image'].to(device)
+            if config['dataset']['mode'] in ['images', 'hybrid']:  
+                image = data['image'].to(device)
             if config['dataset']['mode'] in ['radiomics', 'hybrid']:
                 radiomic_features = data['features'].to(device)
             else:
                 radiomic_features = None
             target_idx = data['label'].to(device)
-
             optimizer.zero_grad()
-            outputs = net(image, radiomic_features)
+                
+            if config['dataset']['mode'] in ['hybrid']:
+                outputs = net(image, radiomic_features)
+            elif config['dataset']['mode'] in ['images']:
+                outputs = net(image)
+            elif config['dataset']['mode'] in ['radiomics']:
+                outputs = net(radiomic_features)
+
             if config['net_train']['criterion'] == 'bce':
                 predicted = torch.round(torch.sigmoid(outputs.reshape(-1)))
                 outputs = torch.sigmoid(outputs).reshape(-1)
@@ -53,13 +60,21 @@ def train_net(net, train_dl, val_dl, criterion, optimizer, n_classes, config, de
         val_total = 0
         with torch.no_grad():
             for i, data in enumerate(val_dl):
-                image = data['image'].to(device)
+                if config['dataset']['mode'] in ['images', 'hybrid']:  
+                    image = data['image'].to(device)
                 if config['dataset']['mode'] in ['radiomics', 'hybrid']:
                     radiomic_features = data['features'].to(device)
                 else:
                     radiomic_features = None
                 target_idx = data['label'].to(device)
-                outputs = net(image, radiomic_features)
+                
+                if config['dataset']['mode'] in ['hybrid']:
+                    outputs = net(image, radiomic_features)
+                elif config['dataset']['mode'] in ['images']:
+                    outputs = net(image)
+                elif config['dataset']['mode'] in ['radiomics']:
+                    outputs = net(radiomic_features)
+
                 if config['net_train']['criterion'] == 'bce':
                     predicted = torch.round(torch.sigmoid(outputs.reshape(-1)))
                     outputs = torch.sigmoid(outputs).reshape(-1)
@@ -69,7 +84,7 @@ def train_net(net, train_dl, val_dl, criterion, optimizer, n_classes, config, de
                 running_val_loss += val_loss.item()
                 val_total += target_idx.size(0)
                 val_correct += (predicted == target_idx).sum().item()
-        
+
         avg_train_loss = running_loss / len(train_dl)
         avg_val_loss = running_val_loss / len(val_dl)
         train_accuracy = correct / total
@@ -80,9 +95,12 @@ def train_net(net, train_dl, val_dl, criterion, optimizer, n_classes, config, de
         print(f"Epoch {epoch+1 :3}/{config['net_train']['epochs']}, train-loss: {avg_train_loss:.3f}, val-loss: {avg_val_loss:.3f}, train-acc: {train_accuracy:.3f}, val-acc: {val_accuracy:.3f}", end=' ')
         print(f"Time taken: {total_time//60:3.0f}m {total_time%60:02.0f}s")
 
-        # Early stopping
+        # Early stopping (min loss or max recall)
+        # recall = recall_score(target_idx.cpu(), predicted.cpu(), average='weighted')
         if avg_val_loss < best_val_loss:
+        # if recall > best_val_metric:
             best_val_loss = avg_val_loss
+            # best_val_metric = recall
             best_model = net.state_dict()
             early_stop_counter = 0
         else:
@@ -102,13 +120,21 @@ def test_net(net, test_dl, config, device):
     true_targets = []
     with torch.no_grad():
         for data in test_dl:
-            image = data['image'].to(device)
+            if config['dataset']['mode'] in ['images', 'hybrid']:  
+                image = data['image'].to(device)
             if config['dataset']['mode'] in ['radiomics', 'hybrid']:
                 radiomic_features = data['features'].to(device)
             else:
                 radiomic_features = None
             target_idx = data['label'].to(device)
-            outputs = net(image, radiomic_features)
+
+            if config['dataset']['mode'] in ['hybrid']:
+                outputs = net(image, radiomic_features)
+            elif config['dataset']['mode'] in ['images']:
+                outputs = net(image)
+            elif config['dataset']['mode'] in ['radiomics']:
+                outputs = net(radiomic_features)
+
             if config['net_train']['criterion'] == 'bce':
                 predicted = torch.round(torch.sigmoid(outputs.reshape(-1)))
             elif config['net_train']['criterion'] == 'ce':
@@ -119,6 +145,7 @@ def test_net(net, test_dl, config, device):
             predicted_targets.extend(predicted.tolist())
             true_targets.extend(target_idx.tolist())
     accuracy = correct / total
+    balanced_accuracy = balanced_accuracy_score(true_targets, predicted_targets)
     print(f"Accuracy: {accuracy}")
     print('Classification Report:')
     print(classification_report(true_targets, predicted_targets))
@@ -126,4 +153,6 @@ def test_net(net, test_dl, config, device):
     print(confusion_matrix(true_targets, predicted_targets))
     
     print('Finished Testing')
-    return classification_report(true_targets, predicted_targets, output_dict=True)
+    report = classification_report(true_targets, predicted_targets, output_dict=True)
+    report['balanced_accuracy'] = balanced_accuracy
+    return report
