5ec3c2a888d4049577b957d59e3a70294dd1222e
rbuler <rafalbuler@gmail.com>
2024-12-11 18:12:48 +0100

    Segmentation masks now are saved with the same size as the images (test set)

--- resize_save.py
+++ resize_save.py
@@ -1,42 +1,77 @@
 import os
-import pandas as pd
 import numpy as np
 from PIL import Image
 import albumentations as A
 # %%
 
-path = '/media/dysk_a/jr_buler/HAM10000/test/ISIC2018_Task3_Test_Images'
-save_path = '/media/dysk_a/jr_buler/HAM10000/test/ISIC2018_Task3_Test_Images_resized'
+'''
+img TRAIN ->                 450 x 600 -> 450 x 450
+seg TRAIN ->                 450 x 600 -> 450 x 450
 
-# Create the save path directory if it doesn't exist
-if not os.path.exists(save_path):
-    os.makedirs(save_path)
+img TEST ->                      450 x 600 -> 450 x 450
+seg TEST ->         224 x 224 -> 450 x 600 -> 450 x 450
 
-# Get a list of all files in the directory
-files = os.listdir(path)
+2x for HAIR and NO_HAIR
 
-# Iterate over the files and load each image
-for file in files:
-    # Skip if file is .DS_Store
-    if file == '.DS_Store':
-        continue
+'''
+
+img_path = '/media/dysk_a/jr_buler/HAM10000/test/ISIC2018_Task3_Test_Images'
+seg_path = '/media/dysk_a/jr_buler/HAM10000/test/ISIC2018_Task3_Test_Images_Segmentations_trained_on_HAM'
+
+img_save_path = '/media/dysk_a/jr_buler/HAM10000/test/new/ISIC2018_Task3_Test_Images'
+seg_save_path = '/media/dysk_a/jr_buler/HAM10000/test/new/ISIC2018_Task3_Test_Images_Segmentations_trained_on_HAM'
+
+os.makedirs(img_save_path, exist_ok=True)
+os.makedirs(seg_save_path, exist_ok=True)
+
+
+image_transform = A.CenterCrop(450, 450)
+mask_upscale_and_crop = A.Compose([
+    A.Resize(450, 600),
+    A.CenterCrop(450, 450)
+])
+
+def process_images(input_path, output_path, transform):
+    files = os.listdir(input_path)
     
-    # Construct the full file path
-    file_path = os.path.join(path, file)
+    for file in files:
+        if file == '.DS_Store':
+            continue
+        
+        file_path = os.path.join(input_path, file)
+
+        image = Image.open(file_path)           
+        image = np.array(image)
+
+        transformed = transform(image=image)
+        processed_image = transformed['image']
+
+        save_file_path = os.path.join(output_path, file)
+        processed_image = Image.fromarray(processed_image)
+        processed_image.save(save_file_path)
+
+
+def process_masks(input_path, output_path, transform):
+    files = os.listdir(input_path)
     
-    # Load the image
-    image = Image.open(file_path)           
-    image = np.array(image)
+    for file in files:
+        if file == '.DS_Store':
+            continue
+        
+        file_path = os.path.join(input_path, file)
+        
+        mask = Image.open(file_path)           
+        mask = np.array(mask)
+        
+        transformed = transform(image=mask)
+        processed_mask = transformed['image']
 
-    # print(image.shape)
-    transform = A.Resize(256, 256)
-    resized_image = transform(image=image)
-    # print(resized_image['image'].shape)
-    resized_image = resized_image['image']
+        save_file_path = os.path.join(output_path, file)
+        processed_mask = Image.fromarray(processed_mask)
+        processed_mask.save(save_file_path)
 
-    save_file_path = os.path.join(save_path, file)
-    # Save the resized image
-    resized_image = Image.fromarray(resized_image)
-    resized_image.save(save_file_path)
+
+process_images(img_path, img_save_path, image_transform)
+process_masks(seg_path, seg_save_path, mask_upscale_and_crop)
     
 # %%
